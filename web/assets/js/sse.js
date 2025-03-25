ead8d7e0f51dc1eb32317305f7f4a7482b6cc837
UnknownMemory <jeremyschneider21@gmail.com>
2025-03-25 14:09:41 +0100

    handle sse message

--- web/assets/js/sse.js
+++ web/assets/js/sse.js
@@ -1,6 +1,13 @@
 const eventSource = new EventSource("http://localhost:8080/api/scoreboard/events");
+
 eventSource.onmessage = function (event) {
-  console.log(event.data);
+  const data = JSON.parse(event.data);
+  for (const key in data) {
+    const element = document.querySelector(`[data-${key}]`);
+    if (element !== null) {
+      element.textContent = data[key];
+    }
+  }
 };
 
 eventSource.onerror = (err) => {
