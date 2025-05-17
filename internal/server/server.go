6d2a0ffeeff2525339cef7f52bdfef3e9b4d8c40
UnknownMemory <jeremyschneider21@gmail.com>
2025-05-17 13:18:15 +0200

    update icon

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,7 +13,7 @@ import (
 
 type Server struct {
 	Theme string `json:"theme"`
-	Port int `json:"port"`
+	Port  int    `json:"port"`
 }
 
 var Assets fs.FS
@@ -22,25 +22,24 @@ var Templates embed.FS
 func (s *Server) Run() {
 	handler.Templates = Templates
 	handler.Theme = s.Theme
-	
+
 	mux := http.NewServeMux()
 
 	assetsFS := http.FileServer(http.FS(Assets))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", disableDirList(assetsFS)))
 
-	themeFS := http.FileServer(http.Dir("./themes/"+s.Theme))
+	themeFS := http.FileServer(http.Dir("./themes/" + s.Theme))
 	mux.Handle("/themes/"+s.Theme+"/", http.StripPrefix("/themes/"+s.Theme+"/", disableDirList(themeFS)))
 
 	mux.HandleFunc("/{$}", handler.Root)
 	mux.HandleFunc("/admin/edit-scoreboard", handler.EditScoreboard)
-	
+
 	mux.HandleFunc("/api/v1/scoreboard/events", handler.SSEEvents)
 	mux.HandleFunc("POST /api/v1/scoreboard/update", handler.SSEUpdate)
 
 	addr := ":" + strconv.Itoa(s.Port)
 	http.ListenAndServe(addr, mux)
 }
-
 
 func (s *Server) Open(url string) error {
 	addr := ":" + strconv.Itoa(s.Port)
@@ -51,14 +50,13 @@ func (s *Server) Open(url string) error {
 	return cmd.Run()
 }
 
-
 func disableDirList(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if strings.HasSuffix(r.URL.Path, "/") {
-            http.NotFound(w, r)
-            return
-        }
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
 
-        handler.ServeHTTP(w, r)
-    })
+		handler.ServeHTTP(w, r)
+	})
 }
